d3d7f2f74924feaeb8a2066bd161f86194cba701
rafaelgss <rafael.nunu@hotmail.com>
2018-06-09 17:21:38 -0300

    capture manager ok

--- managers.py
+++ managers.py
@@ -48,3 +48,89 @@ class CaptureManager(object):
     @property
     def isWritingVideo(self):
         return self._videoFilename is not None
+    
+    def enterFrame(self):
+        """Capture the next frame, if any."""
+
+        # But first, check that any previous frame was exited.
+        assert not self._enteredFrame, \
+        'previous enterFrame() had no matching exitFrame()'
+
+        if self._capture is not None:
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame (self):
+        """Draw to the window. Write to files. Release the
+        frame."""
+
+        # Check whether any grabbed frame is retrievable.
+        # The getter may retrieve and cache the frame.
+        if self.frame is None:
+            self._enteredFrame = False
+            return
+
+        # Update the FPS estimate and related variables.
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        else:
+            timeElapsed = time.time() - self._startTime
+            self._fpsEstimate = self._framesElapsed / timeElapsed
+        self._framesElapsed += 1
+
+        # Draw to the window, if any.
+        if self.previewWindowManager is not None:
+            if self.shouldMirrorPreview:
+                mirroredFrame = numpy.fliplr(self._frame).copy()
+                self.previewWindowManager.show(mirroredFrame)
+            else:
+                self.previewWindowManager.show(self._frame)
+
+        # Write to the image file, if any.
+        if self.isWritingImage:
+            cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+
+        # Write to the video file, if any.
+        self._writeVideoFrame()
+
+        # Release the frame.
+        self._frame = None
+        self._enteredFrame = False
+        
+    def writeImage(self, filename):
+        """Write the next exited frame to an image file."""
+        self._imageFilename = filename
+
+    def startWritingVideo(self, filename,encoding = cv2.VideoWriter_fourcc('I','4','2','0')):
+        """Start writing exited frames to a video file."""
+        self._videoFilename = filename
+        self._videoEncoding = encoding
+    
+    def stopWritingVideo (self):
+        """Stop writing exited frames to a video file."""
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter = None
+
+    def _writeVideoFrame(self):
+        
+        if not self.isWritingVideo:
+            return
+
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0:
+                # The capture's FPS is unknown so use an estimate.
+                if self._framesElapsed < 20:
+                    # Wait until more frames elapse so that the
+                    # estimate is more stable.
+                    return
+                else:
+                    fps = self._fpsEstimate
+            size = (int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_WIDTH)),
+                    int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_HEIGHT)))
+            self._videoWriter = cv2.VideoWriter(self._videoFilename, self._videoEncoding,fps, size)
+
+        self._videoWriter.write(self._frame)
